e8e85a7ac9fbc320f0ae6fcac6036d97b3ca9fa7
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-08-21 11:22:02 +0000

    sdsds New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,heartRate,hrdb,9x,Common,Login,Main,fonts,footer,header,leftnav,neon,rightnav,topnav,pages-config.json] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,googleapis,securityService,WEB-INF,data,i18n,lib,logos,momentLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md] Removed files - [test1,test933]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "neon",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestVCS_098",
+  "homePage" : "Main",
+  "name" : "TestVCS_098",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
